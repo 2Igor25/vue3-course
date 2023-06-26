f36831b36e35884c12e7d01ffc7a12e894ba1bb0
Igor Terekhin <igor.terekhin.02@mail.ru>
2023-06-26 21:10:09 +0500

    Работа с сервером. Получаем посты. Axios. Выпадающий список. Сортировка постов

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,46 @@
 <template>
   <div class="app">
     <h1>Страница с постами</h1>
-
-    <my-button @click="showDialog" style="margin: 15px 0"
-      >Создать пост</my-button
-    >
+    <div class="app_btns">
+      <my-button @click="showDialog">Создать пост</my-button>
+      <my-select v-model="selectedSort" :options="sortOptions" />
+    </div>
     <my-dialog v-model:show="dialogVisible">
       <post-form @create="createPost" />
     </my-dialog>
-    <post-list :posts="posts" @remove="removePost" />
+    <post-list
+      :posts="sortedPosts"
+      @remove="removePost"
+      v-if="!isPostLoading"
+    />
+    <div v-else>Идет загрузка...</div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/MyButton.vue";
+import MySelect from "./components/UI/MySelect.vue";
 
 export default {
   components: {
     PostForm,
     PostList,
     MyButton,
+    MySelect,
   },
   data() {
     return {
-      posts: [
-        { id: 1, title: "Javacript 1", body: "Описание поста 1" },
-        { id: 2, title: "Javacript 2", body: "Описание поста 2" },
-        { id: 3, title: "Javacript 3", body: "Описание поста 3" },
-        { id: 4, title: "Javacript 4", body: "Описание поста 4" },
-      ],
+      posts: [],
       dialogVisible: false,
+      isPostLoading: false,
+      selectedSort: "",
+      sortOptions: [
+        { value: "title", name: "По названию" },
+        { value: "body", name: "По содержимому" },
+      ],
     };
   },
 
@@ -46,6 +55,36 @@ export default {
     showDialog() {
       this.dialogVisible = true;
     },
+    async fetchPosts() {
+      try {
+        this.isPostLoading = true;
+        const response = await axios.get("http://localhost:8080/api/v1/posts");
+        this.posts = response.data;
+        this.isPostLoading = false;
+      } catch (e) {
+        console.log(e);
+        alert("Ошибка");
+      }
+    },
+  },
+  mounted() {
+    this.fetchPosts();
+  },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) => {
+        return post1[this.selectedSort]?.localeCompare(
+          post2[this.selectedSort]
+        );
+      });
+    },
+  },
+  watch: {
+    // selectedSort(newValue) {
+    //   this.posts.sort((post1, post2) => {
+    //     return post1[newValue]?.localeCompare(post2[newValue]);
+    //   });
+    // },
   },
 };
 </script>
@@ -59,5 +98,11 @@ export default {
 
 .app {
   padding: 20px;
+}
+
+.app_btns {
+  margin: 15px 0;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
